8b34043875b3bf59efb5b53e9b97b4f7d8780696
jiaxiaonan <17636154566@163.com>
2020-05-13 11:37:44 +0800

    5.13 新增nodejs的学习文件

--- http-test/app.js
+++ http-test/app.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
     // req是浏览器向服务器发送什么请求，res是服务器返回什么
     console.log(req.method); // 请求方式
     const url = req.url; // 获取请求完整的url
+    // console.log(url)
     req.query = querystring.parse(url.split('?')[1]);    // 解析querystring
     res.end(JSON.stringify(req.query));    // 将querystring返回
 });
