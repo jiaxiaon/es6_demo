cce9120bb50cfb9fe741995cb6f9ceafe0f26493
jiaxiaonan <17636154566@163.com>
2020-09-24 17:05:50 +0800

    9.24

---
+++ TypeScriptDemo/Demo1.ts
@@ -0,0 +1,17 @@
+function jspang(){
+    let web: string = 'Hello World'
+    console.log(web)
+}
+jspang();
+
+const count: number = 1;
+count.toFixed(2);
+
+interface XiaoJieJie {
+    uname: string,
+    age: number
+}
+const xiaohong : XiaoJieJie = {
+    uname: '小红',
+    age: 111
+}
