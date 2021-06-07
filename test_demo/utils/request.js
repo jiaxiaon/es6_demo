bfee2e2a391244f88c491f7c6f191854d76513bf
jiaxiaonan <17636154566@163.com>
2021-06-07 11:12:21 +0800

    6.17 删除vue3test

--- test_demo/utils/request.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const getAxios = () => {
-    // console.log(process.env.API)
-    return new Promise((resolve,reject) => {
-        
-    })
-}
-export default getAxios
