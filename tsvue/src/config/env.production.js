eaa43333e154fc86912afa9e075bfa62866bde0c
jiaxiaonan <17636154566@163.com>
2021-06-08 17:04:29 +0800

    delete tsvue

--- tsvue/src/config/env.production.js
+++
@@ -1,4 +0,0 @@
-// 正式
-module.exports = {
-  baseApi: 'https://magicbox.mobizone.cn/api' // 正式api请求地址
-}
--- tsvue/src/config/env.staging.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // baseApi: 'http://mjnt1.mobizone.cn/api' // 测试api请求地址
-  baseApi: 'https://mjnmdm.mobizone.cn/api'
-}
